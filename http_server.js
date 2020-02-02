062d75d21ec9c21748454fd0b53fca5d84646173
Victor Brender <victor.brender@gmail.com>
2020-02-02 10:46:10 +0100

    Solution for http_server

---
+++ http_server.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var fs = require('fs');
+var through = require('through2');
+var stream = through(write, end);
+
+function write(buffer, encoding, next) {
+  this.push(buffer.toString().toUpperCase());
+  next();
+}
+
+function end(done) {
+  done();
+}
+
+var server = http.createServer(function (req, res) {
+  if (req.method === 'POST') {
+    req.pipe(stream).pipe(res);
+  }
+	res.end('send me a POST\n');
+});
+server.listen(process.argv[2]);
