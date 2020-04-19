66e77e74a8e4bc1cbb7c8277fe1cebc0eebe3388
Florin Oltean <florin.oltean@metoda.com>
2020-04-19 10:19:24 +0200

    Solution for problem 11.

--- functional-javascript-workshop/11-map-with-reduce.js
+++ functional-javascript-workshop/11-map-with-reduce.js
@@ -1,6 +1,24 @@
-module.exports = function arrayMap(arr, fn) {
-    // SOLUTION GOES HERE
+function arrayMap(arr, fn) {
+  return arr.reduce(function(accumulator, element, index, arr) {
+    accumulator.push(fn(element, index, arr));
+    return accumulator;
+  }, []);
 }
+
+module.exports = arrayMap;
+
+function runOurCode() {
+  var nums = [1, 2, 3, 4, 5]
+
+  // `map` is your exported function
+  var output = arrayMap(nums, function double(item) {
+      return item * 2
+  })
+
+  console.log(output) // => [2,4,6,8,10]
+};
+// runOurCode();
+
 
 // # Task
 
