a348484998a6a5b6df9ecb1e40ca6e0f358df256
Florin Oltean <florin.oltean@metoda.com>
2020-05-24 10:13:04 +0200

    Solution for problem 3.

--- promise-it-wont-hurt/02_fulfill_a_promise.js
+++ promise-it-wont-hurt/02_fulfill_a_promise.js
@@ -1,9 +1,3 @@
-// Create a promise. Have it fulfilled with a value of 'FULFILLED!' in
-// executor after a delay of 300ms, using setTimeout.
-
-// Then, print the contents of the promise after it has been fulfilled by passing
-// console.log to then.
-
 'use strict';
 
 var promise = new Promise(function (fulfill, reject) {
