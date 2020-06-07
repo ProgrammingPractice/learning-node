e918174a46b0bd9d9cd13f87e9fcee8d39c83fc6
Florin Oltean <florin.oltean@metoda.com>
2020-06-07 10:44:52 +0200

    Solution for problem 12.

--- promise-it-wont-hurt/11_multiple_promises.js
+++ promise-it-wont-hurt/11_multiple_promises.js
@@ -1,23 +1,3 @@
-// Create a function all that accepts two promises as arguments. This all
-// function should do all of the following:
-
-// Create an internal promise in whatever way you see fit.
-
-// Create a counter variable with initial value of 0.
-
-// Attach then fulfillment handlers to both promises and increment the internal
-// counter when the handlers are called.
-
-// When the counter reaches 2, fulfill the internal promise with an array
-// containing both values.
-
-// Finally return that internal promise to the user.
-
-// After you finish writing your all function, pass getPromise1() and
-// getPromise2() into your new function and then attach console.log as a
-// fulfillment handler to the promise returned by your function. These two
-// promise-returning functions will be provided to you in the global scope.
-
 'use sctrict';
 
 function all(promise1, promise2) {
