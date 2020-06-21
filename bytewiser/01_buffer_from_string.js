a2c98567c65f6d97697e9d24df862efe99e7e009
Florin Oltean <florin.oltean@metoda.com>
2020-06-21 11:23:20 +0200

    Solutions for bytewiser 01 and 02.

---
+++ bytewiser/01_buffer_from_string.js
@@ -0,0 +1,32 @@
+console.log(new Buffer.from("bytewiser"));
+
+//  Buffer From String
+//  Exercise 1 of 7
+
+// In Node.js, Buffer is a global object that provides a way to work with different
+// kinds of binary data. Most APIs in Node core (e.g. http, net, fs) will give you
+// buffers, and will be able to do their job faster if you give them buffers.
+
+// ## Challenge
+
+// Write a node program that prints a buffer object containing
+// the string "bytewiser" using console.log.
+
+// ## Hints
+
+// Read more about Buffers here:
+
+//   [http://](http://)
+
+// Or off-line on your local filesystem:
+
+//   /usr/local/lib/node_modules/bytewiser/docs/node-api-docs.html#all_buffer
+
+// To get output for debugging when running bytewiser run program.js
+// you should use console.error instead of console.log.
+
+
+//  » To print these instructions again, run: bytewiser print
+//  » To execute your program in a test environment, run: bytewiser run program.js
+//  » To verify your program, run: bytewiser verify program.js
+//  » For help run: bytewiser help
